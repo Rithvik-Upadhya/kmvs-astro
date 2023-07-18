93809afdbb32e8c992df47850b5467226bf6fa92
Rithvik <x394@protonmail.com>
2023-07-18 17:56:40 +0530

    added sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://kmvs.org.in",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),
   ],
 });
