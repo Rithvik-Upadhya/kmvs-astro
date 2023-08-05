420690ec945fa7c9da8dd1506da770f074eaea31
Rithvik <x394@protonmail.com>
2023-08-06 00:53:50 +0530

    netlify SSR serverless functions

---
+++ .netlify/functions-internal/chunks/_slug_@_@astro.30caae56.mjs
@@ -0,0 +1,14 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.5f1cc39f.mjs';
+import 'cookie';
+import 'kleur/colors';
+import '@astrojs/internal-helpers/path';
+import 'path-to-regexp';
+import 'mime';
+import 'string-width';
+import 'html-escaper';
+
+const page = () => import('./prerender.5d380f6e.mjs').then(n => n.b);
+
+export { page };
